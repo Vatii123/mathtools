0812670e8680581a081bd8a4e35a2466721ad2a8
vati <66106915+Vatii123@users.noreply.github.com>
2021-05-08 22:37:45 +0700

    implements somethingg (factor finder)
    too lazy to do antoher one kek

--- python/mathtools.py
+++ python/mathtools.py
@@ -1,6 +1,8 @@
+# author : piyaphatliamwilai (https://github.com/piyaphatliamwilai/mathtools & https://github.com/piyaphatliamwilai/somethingg), vatii123 (implements somethingg)
+
 import time
 
-print("4 in 1 Math Solution by https://github.com/piyaphatliamwilai :)")
+print("4 in 1 Math Solution by https://github.com/piyaphatliamwilai & Vatii123 https://github.com/vatii123 :)")
 
 firstNumber = float(input("Give first number (preferably highest number) -> "))
 secondNumber = float(input("Give second number (preferably lowest number) -> "))
@@ -21,6 +23,21 @@ print(divideNumber)
 exponentNumber = firstNumber ** secondNumber;
 print("-------- Exponent --------")
 print(exponentNumber)
-
+# somethingg
+divider = 1
+print("-------- Factor (1) --------")
+while 3 > 2:
+    outputCalculate = firstNumber
+    outputCalculate = outputCalculate / divider
+    numberChecker = (outputCalculate.is_integer())
+    if (numberChecker == True):
+        print(outputCalculate)
+    elif (numberChecker == False):
+        time.sleep(0.000000001)
+    divider = divider + 1
+# check divideNumber
+if (divider == firstNumber):
+  print("finished checking first factor (1) continuing factor 2")
+divider = 1
 while 3 > 2:
     time.sleep(1)
